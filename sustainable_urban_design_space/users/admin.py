73f3aff0849bfc36ba7afd1a1d4101735aeb076c
Max Thakur <Dominick2024204@concordiashanghai.org>
2020-05-03 14:04:49 -0400

    User handler
    Fixing the account handler on the server side.
    
    Co-Authored-By: Brylie Christopher Oxley <brylie@tuxfamily.org>

--- sustainable_urban_design_space/users/admin.py
+++ sustainable_urban_design_space/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    # fields = ('name', 'title', 'view_birth_date')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
