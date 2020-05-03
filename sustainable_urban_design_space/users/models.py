d6c69de971570ed298cb64d5dc920a8763cfbd7c
Max Thakur <Dominick2024204@concordiashanghai.org>
2020-05-03 13:44:15 -0400

    Creating the custom user model
    Yay!
    
    Co-Authored-By: Brylie Christopher Oxley <brylie@tuxfamily.org>

--- sustainable_urban_design_space/users/models.py
+++ sustainable_urban_design_space/users/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class User(User):
+class User(AbstractUser):
     first_name = None
     last_name = None
-    
