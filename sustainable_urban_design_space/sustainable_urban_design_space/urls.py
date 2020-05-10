4a6ab023d2c88078916b4401aba8ff5e2f5bb35d
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-10 20:21:57 +0300

    Add missing imports

--- sustainable_urban_design_space/sustainable_urban_design_space/urls.py
+++ sustainable_urban_design_space/sustainable_urban_design_space/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import include, path, re_path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
