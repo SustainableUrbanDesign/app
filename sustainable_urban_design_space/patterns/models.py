1ac324ccb0a273b81bbbb81fe8568b7d0f3f7024
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-10 20:35:53 +0300

    Add UrbanDesignPattern model

--- sustainable_urban_design_space/patterns/models.py
+++ sustainable_urban_design_space/patterns/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+from wagtail.search import index
+
+
+class UrbanDesignPattern(Page):
+    description = RichTextField()
+
+    search_fields = Page.search_fields + [
+        index.SearchField("description"),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description", classname="full"),
+    ]
