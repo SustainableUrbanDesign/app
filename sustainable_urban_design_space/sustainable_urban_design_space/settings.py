14cacbb2788f14056a0a8226b57f37566c904519
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-10 22:12:48 +0300

    Add templates directory

--- sustainable_urban_design_space/sustainable_urban_design_space/settings.py
+++ sustainable_urban_design_space/sustainable_urban_design_space/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = "sustainable_urban_design_space.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
