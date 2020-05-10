d5f8e1839da2dab20be36776d89f9dc71ce52f48
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-10 22:00:49 +0300

    Add UrbanDesignPatternTag

--- sustainable_urban_design_space/patterns/models.py
+++ sustainable_urban_design_space/patterns/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 from wagtail.search import index
 
 
+class UrbanDesignPatternTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "UrbanDesignPattern", on_delete=models.CASCADE, related_name="tagged_items",
+    )
+
+
 class UrbanDesignPattern(Page):
     description = RichTextField()
+    tags = ClusterTaggableManager(through=UrbanDesignPatternTag, blank=True)
 
     search_fields = Page.search_fields + [
         index.SearchField("description"),
@@ -15,4 +26,5 @@ class UrbanDesignPattern(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel("description", classname="full"),
+        FieldPanel("tags"),
     ]
