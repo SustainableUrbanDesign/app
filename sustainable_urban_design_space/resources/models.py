8bec917765a18065c3542c672d8016846063102e
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-31 14:47:31 +0300

    Initial ResourcesIndex

--- sustainable_urban_design_space/resources/models.py
+++ sustainable_urban_design_space/resources/models.py
@@ -8,3 +8,21 @@ class Book(Page):
     isbn = models.CharField(
         max_length=13, validators=[int_list_validator(sep="-"), MinLengthValidator(9),],
     )
+
+
+class ResourcesIndex(Page):
+    max_count = 1
+
+    parent_page_types = ["home.HomePage"]
+    subpage_types = [
+        "resources.Book",
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        books = Book.objects.all()
+
+        context["books"] = books
+
+        return context
