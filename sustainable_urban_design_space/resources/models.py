3eb2a64664e1f33585a7ed628121f44e2a6d858f
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-31 14:43:18 +0300

    Initial Book model

--- sustainable_urban_design_space/resources/models.py
+++ sustainable_urban_design_space/resources/models.py
@@ -1,3 +1,10 @@
+from django.core.validators import int_list_validator, MinLengthValidator
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+
+
+class Book(Page):
+    isbn = models.CharField(
+        max_length=13, validators=[int_list_validator(sep="-"), MinLengthValidator(9),],
+    )
