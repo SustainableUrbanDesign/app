887131ae88dba30c3549cac8a7b094f2add489e0
Brylie Christopher Oxley <brylie@amble.fi>
2020-05-31 15:00:39 +0300

    Add ISBN field panel and help text

--- sustainable_urban_design_space/resources/models.py
+++ sustainable_urban_design_space/resources/models.py
@@ -1,13 +1,20 @@
 from django.core.validators import int_list_validator, MinLengthValidator
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.models import Page
 
 
 class Book(Page):
     isbn = models.CharField(
-        max_length=13, validators=[int_list_validator(sep="-"), MinLengthValidator(9),],
+        max_length=13,
+        validators=[int_list_validator(sep="-"), MinLengthValidator(9),],
+        help_text="Enter an ISBN (9, 10, or 13) with optional hyphens.",
     )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("isbn"),
+    ]
 
 
 class ResourcesIndex(Page):
@@ -21,8 +28,6 @@ class ResourcesIndex(Page):
     def get_context(self, request):
         context = super().get_context(request)
 
-        books = Book.objects.all()
-
-        context["books"] = books
+        context["books"] = Book.objects.all()
 
         return context
