3019fd82d1e41170fe36cd81ed0f14a28a97c8d7
Brylie Christopher Oxley <brylie@amble.fi>
2020-06-18 22:30:28 +0300

    Render initial map

--- experiments/ui_experiment/src/pages/Index.vue
+++ experiments/ui_experiment/src/pages/Index.vue
@@ -1,14 +1,51 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <MglMap :mapStyle="style" :zoom="zoom" />
   </q-page>
 </template>
 
 <script>
+import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
+
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  components: {
+    MglMap
+  },
+  data() {
+    return {
+      center: [0, 0],
+      zoom: 2,
+      style: {
+        version: 8,
+        sources: {
+          "raster-tiles": {
+            type: "raster",
+            tiles: [
+              "https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg"
+            ],
+            tileSize: 256
+          }
+        },
+        layers: [
+          {
+            id: "simple-tiles",
+            type: "raster",
+            source: "raster-tiles",
+            minzoom: 0,
+            maxzoom: 22
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
+
+<style>
+.mgl-map-wrapper {
+  width: 100vw;
+  height: 90vh;
+}
+</style>
