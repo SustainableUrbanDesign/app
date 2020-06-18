b64afb5fb91725af2d1ade2b4cb0ab0fc8a6d5a6
Brylie Christopher Oxley <brylie@amble.fi>
2020-06-18 22:29:31 +0300

    Lint

--- experiments/ui_experiment/src/router/index.js
+++ experiments/ui_experiment/src/router/index.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import routes from './routes'
+import routes from './routes';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 /*
  * If not building with SSR mode, you can
