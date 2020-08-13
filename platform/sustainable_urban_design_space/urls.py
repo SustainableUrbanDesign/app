2f9ff2058801677fd34409d37bcf0f6d844a4611
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-14 00:17:21 +0300

    Add openstreetmap urls

--- platform/sustainable_urban_design_space/urls.py
+++ platform/sustainable_urban_design_space/urls.py
@@ -22,11 +22,14 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from openstreetmap import urls as openstreetmap_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     re_path(r"^cms/", include(wagtailadmin_urls)),
     re_path(r"^documents/", include(wagtaildocs_urls)),
     re_path(r'^i18n/', include('django.conf.urls.i18n')),
+    path("openstreetmap/", include(openstreetmap_urls)),
     re_path(r"", include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
