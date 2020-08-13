e75c6ba54caa2004f42b318ac99b1554397968b3
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-14 02:03:17 +0300

    Initial working get_osm_data view

--- platform/openstreetmap/views.py
+++ platform/openstreetmap/views.py
@@ -1,6 +1,28 @@
 from django.conf import settings
 from django.http import HttpResponse
 
+import psycopg2
+
+from .queries import create_osm_to_geojson_query
+
 def get_osm_data(request):
-    html = "<p>Hello</p>"
-    return HttpResponse(html)
+    osm_db = settings.DATABASES["openstreetmap"]
+
+    connection = psycopg2.connect(
+        database=osm_db["NAME"],
+        user=osm_db["USER"],
+        password=osm_db["PASSWORD"],
+        host=osm_db["HOST"],
+    )
+
+    cursor = connection.cursor()
+
+    osm_query = create_osm_to_geojson_query(
+        table="osm_points_of_interest"
+    )
+
+    cursor.execute(osm_query)
+
+    response = cursor.fetchone()
+
+    return HttpResponse(response)
