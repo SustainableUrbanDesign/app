4579a758b9d4915b86ec3dd119ecdc4c2ae7775a
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-13 23:56:40 +0300

    Use postgresql

--- platform/sustainable_urban_design_space/settings.py
+++ platform/sustainable_urban_design_space/settings.py
@@ -100,9 +100,13 @@ WSGI_APPLICATION = "sustainable_urban_design_space.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'suds',
+        'USER': 'postgres',
+        'PASSWORD': 'changeme',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
