00dd7863008e9b53bde3ea44150aa6f158962e34
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-14 03:10:09 +0300

    Add TODO

--- platform/openstreetmap/queries.py
+++ platform/openstreetmap/queries.py
@@ -2,6 +2,9 @@ def create_osm_to_geojson_query(table=None, fclass=None, limit=10):
     """
     Create a quuery that returns GeoJSON data from OSM data in PostGIS.
     """
+    # TODO: ensure this query is secured from SQL injection
+    # by using a psycopg2 parameterized query
+
     # TODO: determine how best to allow WHERE clause
     # to filter based on fclass and possibly geometry.
     return f"""
@@ -28,4 +31,3 @@ def create_osm_to_geojson_query(table=None, fclass=None, limit=10):
             ) inputs
         ) features;
     """
-    
