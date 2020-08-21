40ef635012ed8f3ae03f8a5c3d425018846cd04d
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-21 19:37:57 +0300

    Add Project field panels

--- platform/projects/models.py
+++ platform/projects/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.models import Page
 
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('description'),
+    ]
 
     def __str__(self):
         return self.title
