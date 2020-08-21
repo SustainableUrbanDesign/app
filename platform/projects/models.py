ef834863f791ced94818a273314de485c773a00e
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-21 19:34:32 +0300

    Initial Project model

--- platform/projects/models.py
+++ platform/projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from wagtail.core.models import Page
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
