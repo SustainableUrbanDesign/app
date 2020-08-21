cc4c76c4810d82f024b2b0b4f1e6a967990e6f66
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-21 19:38:49 +0300

    Switch description to RichTextField

--- platform/projects/models.py
+++ platform/projects/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = RichTextField()
 
     panels = [
         FieldPanel('title'),
