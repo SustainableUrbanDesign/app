f2d20291b9c04283e2096ef13ba6cfbc58fc8257
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-21 19:57:03 +0300

    Register Project model with Wagtail admin

---
+++ platform/projects/wagtail_hooks.py
@@ -0,0 +1,16 @@
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin, modeladmin_register)
+from .models import Project
+
+
+class ProjectAdmin(ModelAdmin):
+    model = Project
+    menu_icon = 'pilcrow'
+    menu_order = 200
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ('title',)
+    list_filter = ('title',)
+    search_fields = ('title',)
+
+modeladmin_register(ProjectAdmin)
