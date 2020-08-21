fcfb7d9da9c9a02e82dff74a03a49cefc6df93fd
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-21 21:27:43 +0300

    Add link to project view

--- platform/projects/urls.py
+++ platform/projects/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from .views import ProjectListView, ProjectView
 
 urlpatterns = [
-    path("<int:pk>/", ProjectView.as_view()),
+    path(
+        "<int:pk>/",
+        ProjectView.as_view(),
+        name="project_view"
+    ),
     path("", ProjectListView.as_view())
 ]
