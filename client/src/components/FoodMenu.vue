4c24a335dbf46a8a49a46a9ca544cfb33b923459
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-22 22:39:49 +0300

    Add food menu component/data

--- client/src/components/FoodMenu.vue
+++ client/src/components/FoodMenu.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
-    <q-input
-      label="Buffer distance"
-      v-model.number="bufferDistance"
-      type="number"
-      :dense="true"
-      class="q-mx-sm"
-    />
+    <q-input label="Buffer distance" v-model.number="bufferDistance" type="number" :dense="false" />
 
     <q-select
       v-model="bufferUnits"
       :options="bufferUnitsOptions"
       label="Units"
       stack-label
-      :dense="true"
-      :options-dense="true"
+      :dense="false"
+      :options-dense="false"
     />
   </div>
 </template>
+
+<script>
+export default {
+  name: "FoodMenu",
+  data() {
+    return {
+      bufferDistance: 1,
+      bufferUnits: "kilometers",
+      bufferUnitsOptions: ["kilometers", "miles"],
+    };
+  },
+};
+</script>
