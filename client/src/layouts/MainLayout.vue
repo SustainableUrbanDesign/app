8b5c43023e3bb5320253e3fc5f17660657774323
Brylie Christopher Oxley <brylie@amble.fi>
2020-08-22 23:02:12 +0300

    Move footer back to MainLayout

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -128,6 +128,17 @@
     <q-page-container>
       <router-view name="default"></router-view>
     </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
+          </q-avatar>
+          <span>Sustainable Urban Design App</span>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
