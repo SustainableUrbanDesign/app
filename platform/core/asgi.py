37bfd4a2ca3b8c4374df62e1c60d4be8c1fbd552
Brylie Christopher Oxley <brylie@amble.fi>
2020-09-18 19:56:07 +0300

    Rename default project folder to "core"

--- platform/core/asgi.py
+++ platform/core/asgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "sustainable_urban_design_space.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_asgi_application()
