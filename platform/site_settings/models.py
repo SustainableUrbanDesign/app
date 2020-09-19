04e617c648b9f69a5eef001ac1bccd3678d55ffd
Brylie Christopher Oxley <brylie@amble.fi>
2020-09-19 19:13:06 +0300

    Add todo

--- platform/site_settings/models.py
+++ platform/site_settings/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.admin.edit_handlers import FieldPanel
 
-
+# TODO: determine if/how to make Client App Settings model a singleton
+# Note: this may cause problems with Wagtail multi-site
 @register_setting
 class ClientAppSettings(BaseSetting):
     url = models.URLField(help_text="Enter URL for the JavaScript client.")
