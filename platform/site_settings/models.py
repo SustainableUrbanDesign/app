1b745b6ef9880d933d1ab531e961af67257e448c
Brylie Christopher Oxley <brylie@amble.fi>
2020-09-19 18:34:02 +0300

    Add Client App Settings

--- platform/site_settings/models.py
+++ platform/site_settings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+@register_setting
+class ClientAppSettings(BaseSetting):
+    url = models.URLField(help_text="Enter URL for the JavaScript client.")
+
+    panels = [FieldPanel("url")]
+
