ab3c4e584064215db8b9a52807e31169b35a5e12
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-01 16:38:23 +0300

    Add trailing slash

--- platform/projects/urls.py
+++ platform/projects/urls.py
@@ -4,6 +4,6 @@ from .views import EditProjectGoalsView, ProjectListView, ProjectView
 
 urlpatterns = [
     path("<int:pk>/", ProjectView.as_view(), name="project_view"),
-    path("<int:pk>/goals", EditProjectGoalsView.as_view(), name="project_goals_view"),
+    path("<int:pk>/goals/", EditProjectGoalsView.as_view(), name="project_goals_view"),
     path("", ProjectListView.as_view()),
 ]
