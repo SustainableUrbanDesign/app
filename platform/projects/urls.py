ff8e11cf402dbe6bf9466189335e87eed5c99f8f
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-02 20:05:08 +0300

    Give projects list view a name

--- platform/projects/urls.py
+++ platform/projects/urls.py
@@ -5,5 +5,5 @@ from .views import EditProjectGoalsView, ProjectListView, ProjectView
 urlpatterns = [
     path("<int:pk>/", ProjectView.as_view(), name="project_view"),
     path("<int:pk>/goals/", EditProjectGoalsView.as_view(), name="project_goals_view"),
-    path("", ProjectListView.as_view()),
+    path("", ProjectListView.as_view(), name="projects_list"),
 ]
