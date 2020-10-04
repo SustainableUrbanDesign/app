9d3c21b68f93b203f2d3784bd98e8d90ed88347b
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-04 21:15:37 +0300

    Initial design patterns views and new templates

--- platform/patterns/views.py
+++ platform/patterns/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import UrbanDesignPattern
+
+
+class UrbanDesignPatternListView(ListView):
+    model = UrbanDesignPattern
+    context_object_name = "design_patterns"
+    template_name = "patterns/patterns.html"
+
+
+class UrbanDesignPatternView(DetailView):
+    model = UrbanDesignPattern
+    context_object_name = "pattern"
+    template_name = "patterns/pattern.html"
 
