105d80472ad7ce86638797137ebb513119660da8
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-04 22:19:36 +0300

    Clean up UrbanDesignPatternTag URL

--- platform/patterns/urls.py
+++ platform/patterns/urls.py
@@ -3,13 +3,9 @@ from django.urls import path
 from .views import (
     UrbanDesignPatternListView,
     UrbanDesignPatternView,
-    UrbanDesignPatternTagView,
 )
 
 urlpatterns = [
-    path(
-        "tagged/<tag>/", UrbanDesignPatternTagView.as_view(), name="pattern_tag_view",
-    ),
     path("<int:pk>/", UrbanDesignPatternView.as_view(), name="pattern_view"),
     path("", UrbanDesignPatternListView.as_view(), name="patterns_list"),
 ]
