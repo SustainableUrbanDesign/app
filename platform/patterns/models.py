911dfa5d2fb2e7259680275143ac722ce26e9cd9
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-04 22:21:04 +0300

    Remove UrbanDesignPatternTag code

--- platform/patterns/models.py
+++ platform/patterns/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-from taggit.managers import TaggableManager
-
 
 class UrbanDesignPattern(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    tags = TaggableManager()
     image = models.ImageField()
+    # TODO: add tagging
