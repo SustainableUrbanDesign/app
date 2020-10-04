37cd4991ee395ddaa01cccdf02653095d6db1a2f
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-04 22:18:59 +0300

    Remove UrbanDesignPatternTag DetailView

--- platform/patterns/views.py
+++ platform/patterns/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import DetailView, ListView
 
-from .models import UrbanDesignPattern, UrbanDesignPatternTag
+from .models import UrbanDesignPattern
 
 
 class UrbanDesignPatternListView(ListView):
@@ -15,14 +15,3 @@ class UrbanDesignPatternView(DetailView):
     model = UrbanDesignPattern
     context_object_name = "pattern"
     template_name = "patterns/pattern.html"
-
-
-class UrbanDesignPatternTagView(DetailView):
-    model = UrbanDesignPatternTag
-    context_object_name = "tag"
-    template_name = "patterns/tag.html"
-    slug_field = "tag"
-    slug_url_kwarg = "tag"
-
-    def get_object(self):
-        return UrbanDesignPatternTag.objects.get(tag=self.kwargs.get("tag"))
