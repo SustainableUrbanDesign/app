557a03da833c0540ce990c77da0031892df65df2
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-04 21:15:37 +0300

    Initial design patterns views and new templates

---
+++ platform/patterns/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import UrbanDesignPatternListView, UrbanDesignPatternView
+
+urlpatterns = [
+    path("<int:pk>/", UrbanDesignPatternView.as_view(), name="pattern_view"),
+    path("", UrbanDesignPatternListView.as_view(), name="patterns_list"),
+]
