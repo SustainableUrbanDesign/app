7d7bf87bac321fbc5fd2682f721026e2d6f5b294
Brylie Christopher Oxley <brylie@amble.fi>
2020-10-04 22:30:05 +0300

    Add front page view/template

--- platform/front_page/views.py
+++ platform/front_page/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class FrontPageView(TemplateView):
+    template_name = "front_page/front_page.html"
