f44a3a394054e55be160012b40ab6abff7989418
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-11 11:05:56 +0300

    Keywords optional; update UI texts

--- platform/patterns/models.py
+++ platform/patterns/models.py
@@ -7,4 +7,4 @@ class UrbanDesignPattern(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField()
-    keywords = TaggableManager()
+    keywords = TaggableManager(blank=True, verbose_name="Keywords", help_text="A comma-separated list of keywords.")
