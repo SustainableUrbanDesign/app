8a35d6cc2479b8d575fc54a900ce358d0ac8f7ec
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-11 11:02:44 +0300

    Add UrbanDesignPattern.keywords

--- platform/patterns/models.py
+++ platform/patterns/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from taggit.managers import TaggableManager
+
 
 class UrbanDesignPattern(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField()
-    # TODO: add tagging
+    keywords = TaggableManager()
