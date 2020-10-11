72a80691c59613aaa5aca93b91a3a4791174276b
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-11 10:32:08 +0300

    Add pattern form

---
+++ platform/patterns/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from django.contrib.gis.forms import OSMWidget
+
+from .models import UrbanDesignPattern
+
+
+class UrbanDesignPatternForm(ModelForm):
+    class Meta:
+        model = UrbanDesignPattern
+        fields = ("title", "description")
+        
--- platform/patterns/views.py
+++ platform/patterns/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import DetailView, ListView
 
+from .forms import UrbanDesignPatternForm
 from .models import UrbanDesignPattern
 
 
@@ -9,6 +10,26 @@ class UrbanDesignPatternListView(ListView):
     model = UrbanDesignPattern
     context_object_name = "design_patterns"
     template_name = "patterns/patterns.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pattern_form"] = UrbanDesignPatternForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        pattern_form = UrbanDesignPatternForm(request.POST)
+
+        if pattern_form.is_valid():
+            pattern = pattern_form.save()
+
+            return redirect("pattern_view", pk=pattern.pk)
+        else:
+            context = {
+                "pattern_form": pattern_form,
+            }
+            
+            return render(request, self.template_name, context)
 
 
 class UrbanDesignPatternView(DetailView):
