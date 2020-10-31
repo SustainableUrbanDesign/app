eacc7d5eb77f534eeafc19897cf88a223ddb713f
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-31 17:17:24 +0200

    Rename view to project_detail_view

--- platform/projects/urls.py
+++ platform/projects/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import EditProjectGoalsView, ProjectListView, ProjectDetailView
+from .views import (
+    EditProjectGoalsView,
+    ProjectListView,
+    ProjectDetailView,
+)
 
 urlpatterns = [
-    path("<int:pk>/", ProjectDetailView.as_view(), name="project_view"),
+    path("<int:pk>/", ProjectDetailView.as_view(), name="project_detail_view"),
     path("<int:pk>/goals/", EditProjectGoalsView.as_view(), name="project_goals_view"),
     path("", ProjectListView.as_view(), name="projects_list"),
 ]
