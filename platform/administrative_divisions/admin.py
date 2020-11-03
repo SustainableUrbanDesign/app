423e3e84d241aefe0af6ecbf36096dbce96bbdce
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-03 20:28:18 +0200

    REgister AdministrativeDivision with Django admin

--- platform/administrative_divisions/admin.py
+++ platform/administrative_divisions/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
-# Register your models here.
+from .models import AdministrativeDivision
+
+
+@admin.register(AdministrativeDivision)
+class AdministrativeDivisionAdmin(admin.OSMGeoAdmin):
+    pass
