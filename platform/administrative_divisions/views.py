4d87bf8ee38154ab9a6a83d8fda720900fea501a
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-03 21:11:34 +0200

    Add AdministrativeDivision CRUD views

--- platform/administrative_divisions/views.py
+++ platform/administrative_divisions/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView
 
-# Create your views here.
+from .models import AdministrativeDivision
+
+
+class AdministrativeDivisionListView(ListView):
+    model = AdministrativeDivision
+    context_object_name = "administrative_divisions"
+    template_name = "administrative_divisions/administrative_divisions.html"
+
+
+class AdministrativeDivisionDetailView(DetailView):
+    model = AdministrativeDivision
+    context_object_name = "administrative_division"
+    template_name = "administrative_divisions/administrative_division.html"
+
+
+class AdministrativeDivisionCreateView(CreateView):
+    model = AdministrativeDivision
+    template_name = "administrative_divisions/administrative_division_form.html"
+    fields = [
+        "name",
+        "description",
+        "website",
+        "division_type",
+    ]
+
+
+class AdministrativeDivisionUpdateView(UpdateView):
+    model = AdministrativeDivision
+    context_object_name = "administrative_division"
+    template_name = "administrative_divisions/administrative_division_form.html"
+    fields = [
+        "name",
+        "description",
+        "website",
+        "division_type",
+    ]
