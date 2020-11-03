086fe0e9558705f150ad42ca6e90350267382fa5
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-03 20:28:36 +0200

    Initial AdministrativeDivision model

--- platform/administrative_divisions/models.py
+++ platform/administrative_divisions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class DivisionTypeChoices(models.TextChoices):
+    DISTRICT = "district", _("District")
+    MUNICIPALITY = "municipality", _("Municipality")
+
+
+class AdministrativeDivision(MPTTModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    website = models.URLField()
+    division_type = models.CharField(max_length=255, choices=DivisionTypeChoices.choices)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True, related_name="children"
+    )
