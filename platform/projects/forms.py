d5c9930085389ae44f0d2253015a8cd3bc8a4419
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-15 21:12:23 +0200

    Include administrative_division field

--- platform/projects/forms.py
+++ platform/projects/forms.py
@@ -7,7 +7,7 @@ from .models import Project
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ("title", "description", "geographic_area")
+        fields = ("title", "description", "geographic_area", "administrative_division")
         widgets = {
             "geographic_area": forms.HiddenInput
         }
