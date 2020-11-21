2052573700b8e1b1a9c6dc7eae4f220f3522963c
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-21 19:37:21 +0200

    Initial UserProfile model

--- platform/users/models.py
+++ platform/users/models.py
@@ -5,3 +5,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = None
     last_name = None
+
+
+class UserProfile(models.Model):
+    family_name = models.CharField(max_length=255, null=True, blank=True)
+    given_name = models.CharField(max_length=255, null=True, blank=True)
+    job_title = models.CharField(max_length=255, null=True, blank=True)
