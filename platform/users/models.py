196228b03509c1bdc604ecb6a4f9b82c79792a3d
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-21 19:39:54 +0200

    Add User.profile field

--- platform/users/models.py
+++ platform/users/models.py
@@ -5,6 +5,13 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = None
     last_name = None
+    profile = models.ForeignKey(
+        to="users.UserProfile",
+        null=True,
+        blank=True,
+        related_name="user",
+        on_delete=models.SET_NULL,
+    )
 
 
 class UserProfile(models.Model):
