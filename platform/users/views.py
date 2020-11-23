b1cc7156fdf416297b50d7a4b43f071f363ef96d
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-23 12:18:34 +0200

    Initial UserProfile CRUD

--- platform/users/views.py
+++ platform/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView
 
-# Create your views here.
+#from .forms import UserProfileForm
+from .models import UserProfile
+
+
+class UserProfileListView(ListView):
+    model = UserProfile
+    context_object_name = "profiles"
+    template_name = "users/profiles.html"
+
+
+class UserProfileDetailView(DetailView):
+    model = UserProfile
+    context_object_name = "profile"
+    template_name = "users/profile.html"
+
+
+class UserProfileCreateView(CreateView):
+    model = UserProfile
+    template_name = "users/profile_form.html"
+    #form_class = UserProfileForm
+    fields = ("given_name", "family_name", "job_title")
+
+
+class UserProfileUpdateView(UpdateView):
+    model = UserProfile
+    context_object_name = "profile"
+    template_name = "users/profile_form.html"
+    #form_class = UserProfileForm
+    fields = ("given_name", "family_name", "job_title")
